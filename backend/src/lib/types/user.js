45045873532550b958adac7da57c2951304f35d6
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-10 12:33:34 -0500

    [FIX] bug typo fields

--- backend/src/lib/types/user.js
+++ backend/src/lib/types/user.js
@@ -6,7 +6,7 @@ const {
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  filed: {
+  fields: {
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     lastName: { type: GraphQLString },
