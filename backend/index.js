29d341920261bee95c272e8d2481440f967968b9
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-10 11:39:04 -0500

    [ADD] type user graphql

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,20 @@
 const express = require('express')
-const expressGraphql = require('express-graphql')
+const expressGraphql = require('express-graphql').graphqlHTTP
 const cors = require('cors')
 
 const db = require('./database')
 const { port } = require('./config')
 
 const app = express()
-
 db.connect()
 
-// const UserSchema = require('')
+const UserSchema = require('./src/lib/schemas/user')
 
-// app.use(cors())
-// // app.use('/graphql', expressGraphql({
-// //   schema: UserSchema,
-// //   graphiql: true,
-// // }))
+app.use('*', cors())
+app.use('/graphql', expressGraphql({
+  schema: UserSchema,
+  graphql: true,
+}))
 
 app.listen(port, () => {
   console.log(`Listening on: http://localhost:${port}`)
---
+++ backend/src/lib/types/user.js
@@ -0,0 +1,19 @@
+const {
+  GraphQLID,
+  GraphQLString,
+  GraphQLObjectType,
+} = require('graphql')
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  filed: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+    role: { type: GraphQLString },
+  }
+})
+
+module.exports = UserType
