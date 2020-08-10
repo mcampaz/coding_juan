87939bee015596f9d4b057dc681ca090366de786
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-10 12:52:38 -0500

    [FEATURE] create and get users

--- backend/src/lib/schemas/user.js
+++ backend/src/lib/schemas/user.js
@@ -49,108 +49,104 @@ const UserSchema = {
           },
           role: {
             type: new GraphQLNonNull(GraphQLString)
-          },
-          async resolve(root, args, context, info) {
-            const salt = await bcrypt.genSalt(10)
-            if (!salt) {
-              throw new Error('Error generating salt')
-            }
-
-            const passEncript = await bcrypt.hash(args.password, salt)
-            if (!passEncript) {
-              throw new Error('Error encrypting password')
-            }
-            args.password = passEncript
-
-            const newUserModel = await new UserModel(args)
-            const newUser = await newUserModel.save()
-            if (!newUser) {
-              throw new Error('Error')
-            }
-            return newUser
           }
         },
-        updateUser: {
-          type: UserType,
-          args: {
-            id: {
-              name: '_id',
-              type: GraphQLNonNull(GraphQLString)
-            },
-            name: {
-              type: GraphQLString
-            },
-            lastName: {
-              type: GraphQLString
-            },
-            email: {
-              type: GraphQLString
-            },
-            password: {
-              type: GraphQLString
-            },
-            role: {
-              type: GraphQLString
-            }
+        async resolve(root, args, context, info) {
+          const salt = await bcrypt.genSalt(10)
+          if (!salt) {
+            throw new Error('Error generating salt')
+          }
+
+          const passEncript = await bcrypt.hash(args.password, salt)
+          if (!passEncript) {
+            throw new Error('Error encrypting password')
+          }
+          args.password = passEncript
+
+          const newUserModel = await new UserModel(args)
+          const newUser = await newUserModel.save()
+          if (!newUser) {
+            throw new Error('Error')
+          }
+          return newUser
+        }
+      },
+      updateUser: {
+        type: UserType,
+        args: {
+          name: {
+            type: GraphQLString
           },
-          async resolve(root, args, context, info) {
-            const userToUpdate = await UserModel.findById(args.id)
-            if (!userToUpdate) {
-              throw new Error('Does no exist the user')
-            }
-
-            for (let property in args) {
-              if (args[property] != userToUpdate[property] && args[property] != '') {
-                userToUpdate[property] = args[property]
-              }
-            }
-
-            const userUpdated = await userToUpdate.save()
-            if (!userUpdated) {
-              throw new Error('Error updating user')
-            }
-
-            return userUpdated
+          lastName: {
+            type: GraphQLString
+          },
+          email: {
+            type: GraphQLString
+          },
+          password: {
+            type: GraphQLString
+          },
+          role: {
+            type: GraphQLString
           }
         },
-        removeUser: {
-          type: UserType,
-          args: {
-            id: { type: new GraphQLNonNull(GraphQLString) }
-          },
-          async resolve(root, args, context, info) {
-            const removeUser = await UserModel.findByIdAndRemove(args.id).exec()
-            if (!removeUser) {
-              throw new Error('Error')
-            }
-
-            return removeUser
+        async resolve(root, args, context, info) {
+          const userToUpdate = await UserModel.findById(args.id)
+          if (!userToUpdate) {
+            throw new Error('Does no exist the user')
           }
+
+          for (let property in args) {
+            if (args[property] != userToUpdate[property] && args[property] != '') {
+              userToUpdate[property] = args[property]
+            }
+          }
+
+          const userUpdated = await userToUpdate.save()
+          if (!userUpdated) {
+            throw new Error('Error updating user')
+          }
+
+          return userUpdated
+        }
+      },
+      removeUser: {
+        type: UserType,
+        args: {
+          id: { type: new GraphQLNonNull(GraphQLString) }
         },
-        login: {
-          type: UserType,
-          args: {
-            email: { type: GraphQLNonNull(GraphQLString) },
-            password: { type: GraphQLNonNull(GraphQLString) }
-          },
-          async resolve(root, args, context, info) {
-            const user = await UserModel.findOne({ email: args.email })
-            if (!user) {
-              throw new Error('Cant find email')
-            }
+        async resolve(root, args, context, info) {
+          const removeUser = await UserModel.findByIdAndRemove(args.id).exec()
+          if (!removeUser) {
+            throw new Error('Error')
+          }
 
-            const secure = await bcrypt.compare(args.password, user.password)
-            if (!secure) {
-              throw new Error('Wrong password')
-            }
+          return removeUser
+        }
+      },
+      login: {
+        type: UserType,
+        args: {
+          email: { type: GraphQLNonNull(GraphQLString) },
+          password: { type: GraphQLNonNull(GraphQLString) }
+        },
+        async resolve(root, args, context, info) {
+          const user = await UserModel.findOne({ email: args.email })
+          if (!user) {
+            throw new Error('Cant find email')
+          }
 
-            return {
-              id: user._id,
-              name: user.name,
-              lastName: user.lasName,
-              email: user.email,
-              role: user.role
-            }
+          const secure = await bcrypt.compare(args.password, user.password)
+          if (!secure) {
+            throw new Error('Wrong password')
+          }
+
+          return {
+            id: user._id,
+            name: user.name,
+            lastName: user.lasName,
+            email: user.email,
+            role: user.role
           }
         }
       }
